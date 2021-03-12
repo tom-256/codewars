24d8ef776c33bd9bdd2d0fd4f959a6b08440423c
tom-256 <t11a.256.512.1024@gmail.com>
2021-03-12 21:08:03 +0900

    RGB To Hex Conversion

--- rgb_to_hex_conversion/index.ts
+++ rgb_to_hex_conversion/index.ts
@@ -1,15 +1,7 @@
 export const rgb = (r: number, g: number, b: number): string => {
-  return `${padding(round(r).toString(16).toUpperCase())}${padding(
-    round(g).toString(16).toUpperCase()
-  )}${padding(round(b).toString(16).toUpperCase())}`;
-};
-const round = (x: number): number => {
-  if (x > 255) return 255;
-  if (x < 0) return 0;
-  return x;
-};
-
-const padding = (x: string): string => {
-  if (x.length === 1) return x.padStart(2, "0");
-  return x;
+  return [r, g, b]
+    .map((x) =>
+      Math.min(Math.max(x, 0), 255).toString(16).padStart(2, "0").toUpperCase()
+    )
+    .join("");
 };
